1526ff4af20ae5df5e7b42931aa1d0b42000f740
crazycookie1103 <avnikhanulia@gmail.com>
2025-09-04 18:19:55 +0530

    Update project.py

--- healthcare/project.py
+++ healthcare/project.py
@@ -28,7 +28,7 @@ st.set_page_config(
     layout="centered"
 )
 
-st.title("❤️ Heart Disease Prediction App")
+st.title("❤️ Heart Disease Prediction")
 st.markdown("""
 Enter your details below and find out your risk of heart disease.
 """)
@@ -79,3 +79,4 @@ if st.button("Predict Heart Disease"):
 
 # ---- Footer ----
 st.markdown("---")
+
